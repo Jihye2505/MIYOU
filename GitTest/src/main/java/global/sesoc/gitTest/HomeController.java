c1ba8d4a9a98855b136b9866b4cfc5dadf52927f
kita <kita@kita-PC>
2017-03-20 10:42:04 +0900

    home

--- GitTest/src/main/java/global/sesoc/gitTest/HomeController.java
+++ GitTest/src/main/java/global/sesoc/gitTest/HomeController.java
@@ -6,22 +6,22 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
+
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
@@ -36,4 +36,11 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String insertMeeting(Model model) {
+		
+		
+		
+		return "meeting";
+	}
 }
